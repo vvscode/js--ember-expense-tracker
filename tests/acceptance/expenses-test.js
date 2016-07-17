93cbe01e54b2eebfec36eaa1f43e84717253a2df
Vasiliy Vanchuk <v.vanchuk@tut.by>
2016-07-17 14:48:11 +0300

    Expenses should printout list of expenses : expenses-list

--- tests/acceptance/expenses-test.js
+++ tests/acceptance/expenses-test.js
@@ -1,6 +1,13 @@
-/* global visit, andThen, currentURL */
-import { test } from 'qunit';
+/* global visit, andThen, currentURL, findWithAssert */
+import Ember from 'ember';
+import {test} from 'qunit';
 import moduleForAcceptance from 'js--ember-expense-tracker/tests/helpers/module-for-acceptance';
+
+const {
+  run
+} = Ember;
+
+const generateExpenseTitle = (i) => `Expense title #${i}`;
 
 let store;
 
@@ -18,5 +25,30 @@ test('visiting /expenses', function(assert) {
 
   andThen(function() {
     assert.equal(currentURL(), '/expenses');
+  });
+});
+
+test('/expenses should display list of expenses', function(assert) {
+  let objectsCount = 5;
+  run(() => {
+    for (let i = 0; i < objectsCount; i++) {
+      store.createRecord('expense', {
+        amount: i * 5,
+        category: null,
+        date: new Date(),
+        description: `Expense description #${i}`,
+        title: generateExpenseTitle(i)
+      });
+    }
+  });
+
+  visit('/expenses');
+
+  andThen(function() {
+    let el = findWithAssert('.qa--expense-item');
+    assert.equal(el.length, objectsCount, `Should contains ${objectsCount} items`);
+    for (let i = 0; i < objectsCount; i++) {
+      assert.equal(el.get(i).textContent, generateExpenseTitle(i), `Should display title for expense ${i}`);
+    }
   });
 });
